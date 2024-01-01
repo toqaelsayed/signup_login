f2ffbb68eae4b0b2586ea4a9ba238a40552d59bc
Omar Ahmed <eng.omarahmed@outlook.com>
2024-01-01 23:43:22 +0200

    last updated

---
+++ lib/Screens/Signup/components/socal_sign_up.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Signup/components/social_icon.dart';
+
+import '../../../screens/Signup/components/or_divider.dart';
+
+class SocalSignUp extends StatelessWidget {
+  const SocalSignUp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const OrDivider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SocalIcon(
+              iconSrc: "assets/icons/facebook.svg",
+              press: () {},
+            ),
+            SocalIcon(
+              iconSrc: "assets/icons/twitter.svg",
+              press: () {},
+            ),
+            SocalIcon(
+              iconSrc: "assets/icons/google-plus.svg",
+              press: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
